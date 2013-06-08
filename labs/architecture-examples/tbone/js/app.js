0fe6f7f444e624e3a661a9315feb34571cc729e2
Dan Riti <dmriti@gmail.com>
2013-06-08 14:38:47 -0400

    First pass, TBONE IS HARD!

--- labs/architecture-examples/tbone/js/app.js
+++ labs/architecture-examples/tbone/js/app.js
@@ -3,4 +3,17 @@
 
 	// Your starting point. Enjoy the ride!
 
+    // Models
+    tbone.models.Foo = tbone.models.base.extend({
+        state: function() {
+            return {bar: 'Hello world!'};
+        }
+    });
+
+    // Instances
+    tbone('foo', tbone.models.Foo.make());
+
+    // Let's rock!
+    tbone.render(jQuery('[tbone]'));
+
 })( window );
