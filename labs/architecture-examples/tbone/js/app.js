028e9387cff2eb2e803bd6bd1c882f51bfc60517
Dan Riti <driti@tracelytics.com>
2013-06-11 19:47:09 +0000

    Update to make templates work!

--- labs/architecture-examples/tbone/js/app.js
+++ labs/architecture-examples/tbone/js/app.js
@@ -2,16 +2,27 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+    var todo = {};
 
     // Models
-    tbone.models.Foo = tbone.models.base.extend({
+    todo.Foo = tbone.models.base.extend({
         state: function() {
-            return {bar: 'Hello world!'};
+            return {bar: 'Hello world!',
+                    lol: 'Templates work!'};
         }
     });
 
     // Instances
-    tbone('foo', tbone.models.Foo.make());
+    tbone('foo', todo.Foo.make());
+
+    // Magic to make template scripts work! Need to work with Tillberg to get
+    // this somewhat formalized. What are we going to do with this?
+    $('script[type="text/template"]').each(function() {
+        var $this = $(this);
+        var html = $this.html();
+        var name = $this.data('name');
+        tbone.addTemplate(name, html);
+    });
 
     // Let's rock!
     tbone.render(jQuery('[tbone]'));
