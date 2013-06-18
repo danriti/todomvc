577aea72c283ed9309b3636a3a616ca5a66ad75e
Dan Riti <dmriti@gmail.com>
2013-06-17 23:01:26 -0400

    Add basic list support.

--- labs/architecture-examples/tbone/js/app.js
+++ labs/architecture-examples/tbone/js/app.js
@@ -2,18 +2,33 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-    var todo = {};
+    var app = {};
 
     // Models
-    todo.Foo = tbone.models.base.extend({
+    app.Foo = tbone.models.base.extend({
         state: function() {
             return {bar: 'Hello world!',
                     lol: 'Templates work!'};
         }
     });
 
+    app.Todo = tbone.models.base.extend({
+        state: function() {
+            return {
+                title: this.title,
+                checked: false
+            }
+        }
+    });
+
     // Instances
-    tbone('foo', todo.Foo.make());
+    tbone('touch', 1);
+    tbone('todos', []);
+
+    T(function() {
+        var touch = tbone.query('touch');
+        tbone.push('todos', app.Todo.make({title: touch}));
+    });
 
     // Let's rock!
     tbone.render(jQuery('[tbone]'));
